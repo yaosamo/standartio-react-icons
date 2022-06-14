665e9f5d2500ac0d7f847f73bc36506d7bff3acd
Shola Anozie <shola@soulprovidr.fm>
2022-06-13 23:33:44 -0400

    Exporting icons from Figma. (#1)
    * Configured Figma export.
    
    * Exporting icons as SVGR.
    
    * Now exporting React components from dist/icons folder.
    
    * Added new line.
    
    * Set Figma page name
    
    * Fixed access token env variable.
    
    * Now setting stroke to currentColor, instead of fill.
    
    * Added initial GitHub Action to build icons on push.
    
    * Fixed action.
    
    * Added env variables.
    
    * Fixed env configuration.
    
    * Cleaned up .gitignore.
    
    * Push commit to main.
    
    * Configure actions/checkout to fix committing to branch.
    
    * Only build icons when PR is merged.

---
+++ figma.config.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+
+const outputComponentsAsSvgr = require("@figma-export/output-components-as-svgr");
+const transformSvgWithSvgo = require("@figma-export/transform-svg-with-svgo");
+
+const fileId = process.env.FIGMA_FILE_ID;
+const pageName = process.env.FIGMA_PAGE_NAME;
+
+const svgoConfig = {
+  multipass: true,
+  plugins: [
+    "preset-default",
+    "removeDimensions",
+    {
+      name: "removeAttrs",
+      params: {
+        attrs: ["stroke"],
+      },
+    },
+    {
+      name: "addAttributesToSVGElement",
+      params: {
+        attributes: [
+          {
+            stroke: "currentColor",
+          },
+        ],
+      },
+    },
+    {
+      name: "sortAttrs",
+      params: {
+        xmlnsOrder: "alphabetical",
+      },
+    },
+  ],
+};
+
+module.exports = {
+  commands: [
+    [
+      "components",
+      {
+        fileId,
+        onlyFromPages: [pageName],
+        transformers: [transformSvgWithSvgo(svgoConfig)],
+        outputters: [
+          outputComponentsAsSvgr({
+            output: "./dist",
+            getSvgrConfig: () => ({
+              jsxRuntime: "automatic",
+            }),
+          }),
+        ],
+      },
+    ],
+  ],
+};
